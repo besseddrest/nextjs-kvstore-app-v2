822b10ee34826375cdafe6f009cca42125bdf848
Harold C <hcabalic@gmail.com>
2023-03-28 22:47:37 -0700

    Add react-hook-form

--- pages/form/index.tsx
+++ pages/form/index.tsx
@@ -1,8 +1,17 @@
 import Layout from '../../components/layout';
+import { useForm } from 'react-hook-form';
+
 export default function Form() {
+  const { handleSubmit, register } = useForm({mode: 'onChange'});
+  
+  const handleRegistration = (data) => console.log(data);
   return (
     <Layout>
-      <p>Form goes here</p>
+      <form onSubmit={handleSubmit(handleRegistration)}>
+        <input {...register('userName')} type="text" placeholder="e.g. hcabalic" />
+        <input {...register('datasetName')} type="text" placeholder="e.g. my_dataset" />
+        <button type="submit">Submit Request</button>
+      </form>
     </Layout>
   )
 }
