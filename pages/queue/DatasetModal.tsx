88e19684a27666d670bc2b47c4a6767351bcfd4b
Harold C <hcabalic@gmail.com>
2023-07-17 17:41:21 -0700

    minor

--- pages/queue/DatasetModal.tsx
+++ pages/queue/DatasetModal.tsx
@@ -9,16 +9,16 @@ export default function DatasetModal(props: Props) {
   const { selectedDataset, dialogRef, handleDialogClick } = props;
   return (
     <dialog id="selected" ref={dialogRef} onClick={handleDialogClick}>
-        {
-          selectedDataset && 
-            <>
-              {
-                Object.entries(selectedDataset).map((item, i) => 
-                  <p key={`metadata-row-${i}`}><strong>{item[0]}:</strong> {item[1]}</p>
-                )
-              }
-            </>
-        }
-      </dialog>
+      {
+        selectedDataset && 
+          <>
+            {
+              Object.entries(selectedDataset).map((item, i) => 
+                <p key={`metadata-row-${i}`}><strong>{item[0]}:</strong> {item[1]}</p>
+              )
+            }
+          </>
+      }
+    </dialog>
   )
 }
